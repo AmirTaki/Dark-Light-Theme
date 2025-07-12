53eb7a7b1adfcff2b75c463ed1f4b2394867849e
amir <amirtaki623@gmail.com>
2025-07-12 23:37:32 +0330

    toggle listener p1

--- src/main.js
+++ src/main.js
@@ -1,2 +1,10 @@
 const toggle = document.getElementById("toggleDark")
 const body = document.querySelector('body')
+
+
+{/* <i class="bi bi-moon-fill"></i>/ */}
+
+toggle.addEventListener("click", (e)=> {
+  toggle.classList.toggle('bi-moon-fill')  
+ 
+})
